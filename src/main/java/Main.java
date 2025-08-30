c766553969f6afe16ee85fb6c1edb0c07ee5e12b
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-08-30 21:40:17 +0300

    demo code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,11 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         EmployeeService employeeService = context.getBean("employeeService", EmployeeService.class);
+
+        Employee employee = new Employee(11, "Employee 11");
+        employeeService.save(employee);
+        employeeService.deleteById(1);
         System.out.println(employeeService.findAll());
+        System.out.println(employeeService.findById(5));
     }
 }
