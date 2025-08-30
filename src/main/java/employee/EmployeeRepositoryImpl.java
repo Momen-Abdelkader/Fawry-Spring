67f25f2afc686b7cf6a3f1c0bffc43715bcf25c6
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-08-30 21:36:26 +0300

    cleaup

--- src/main/java/employee/EmployeeRepositoryImpl.java
+++ src/main/java/employee/EmployeeRepositoryImpl.java
@@ -31,18 +31,4 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
     public void deleteById(int id) {
         employees.remove(id);
     }
-
-//    @PostConstruct
-//    public void populateData() {
-//        employees.put(1, new Employee(1, "Employee 1"));
-//        employees.put(2, new Employee(2, "Employee 2"));
-//        employees.put(3, new Employee(3, "Employee 3"));
-//        employees.put(4, new Employee(4, "Employee 4"));
-//        employees.put(5, new Employee(5, "Employee 5"));
-//        employees.put(6, new Employee(6, "Employee 6"));
-//        employees.put(7, new Employee(7, "Employee 7"));
-//        employees.put(8, new Employee(8, "Employee 8"));
-//        employees.put(9, new Employee(9, "Employee 9"));
-//        employees.put(10, new Employee(10, "Employee 10"));
-//    }
 }
